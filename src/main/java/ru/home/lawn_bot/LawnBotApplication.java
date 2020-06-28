b86d5d00c5b7a1f4dcb29cc9ba7e3a9189f86ef1
Paul <skrib3@yandex.ru>
2020-06-28 05:41:07 +0300

    turned off proxy settings turned on ping task for heroku

--- src/main/java/ru/home/lawn_bot/LawnBotApplication.java
+++ src/main/java/ru/home/lawn_bot/LawnBotApplication.java
@@ -2,11 +2,16 @@ package ru.home.lawn_bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.telegram.telegrambots.ApiContextInitializer;
 
+@EnableScheduling
 @SpringBootApplication
 public class LawnBotApplication {
 
     public static void main(String[] args) {
+
+        ApiContextInitializer.init();
 
         SpringApplication.run(LawnBotApplication.class, args);
     }
